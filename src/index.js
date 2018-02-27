70192b8327bd1f61fd8c54f15edfe37f6ba5a4f6
artyom123 <artvirsh@mail.ru>
2018-02-27 23:26:00 +0300

    smart-calculator decided

--- src/index.js
+++ src/index.js
@@ -1,26 +1,54 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.initialValue = initialValue;
+    this.arr = [initialValue];
+    this.result = 0;
+    this.k = 2;
   }
 
   add(number) {
-    // your implementation
+    this.arr.push('+');
+    this.arr.push(number);
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.arr.push('-');
+    this.arr.push(number);
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.arr.push('*');
+    this.arr.push(number);
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.arr.push('/');
+    this.arr.push(number);
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    let arrNum,
+        str = this.arr.pop();
+
+    if(typeof str === 'string'){
+        arrNum = str.match(/(\d)+/g);
+        str = str.slice(0,str.length - this.k );
+        this.arr.push(str+'Math.pow('+arrNum[arrNum.length-1]+','+number+')' + ')'.repeat(this.k-1));
+        this.k++;
+    }
+    else{ 
+        this.arr.push('Math.pow('+str+', '+number+')');
+        this.k = 2;
+    }
+    return this;
+  }
+  
+  toString(){
+    return eval(this.arr.join(''));
   }
 }
 
