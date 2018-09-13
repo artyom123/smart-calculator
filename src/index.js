60b82cfb8e462e1f2144eb98e42f6c4bb4c0deda
Artyom <artvirsh@mail.ru>
2018-09-13 17:04:32 +0300

    Edit desicion

--- src/index.js
+++ src/index.js
@@ -1,54 +1,54 @@
 class SmartCalculator {
   constructor(initialValue) {
-    this.initialValue = initialValue;
     this.arr = [initialValue];
-    this.result = 0;
-    this.k = 2;
+    this.count = 0;
   }
 
   add(number) {
+    this.addBrackets();
     this.arr.push('+');
     this.arr.push(number);
     return this;
   }
   
   subtract(number) {
+    this.addBrackets();
     this.arr.push('-');
     this.arr.push(number);
     return this;
   }
 
   multiply(number) {
+    this.addBrackets();
     this.arr.push('*');
     this.arr.push(number);
     return this;
   }
 
   devide(number) {
+    this.addBrackets();
     this.arr.push('/');
     this.arr.push(number);
     return this;
   }
 
   pow(number) {
-    let arrNum,
-        str = this.arr.pop();
-
-    if(typeof str === 'string'){
-        arrNum = str.match(/(\d)+/g);
-        str = str.slice(0,str.length - this.k );
-        this.arr.push(str+'Math.pow('+arrNum[arrNum.length-1]+','+number+')' + ')'.repeat(this.k-1));
-        this.k++;
-    }
-    else{ 
-        this.arr.push('Math.pow('+str+', '+number+')');
-        this.k = 2;
-    }
+    this.arr.push(`Math.pow(${this.arr.pop()},`);
+    this.arr.push(number);
+    this.count++;
     return this;
   }
   
   toString(){
+    this.addBrackets();
     return eval(this.arr.join(''));
+  }
+
+  addBrackets() {
+    if(this.count > 0) {
+      this.arr.push(')'.repeat(this.count));
+      this.count = 0;
+    }
   }
 }
 
